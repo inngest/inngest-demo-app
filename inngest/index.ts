4a6c812653d6876b5217050e7059a21bb28b7297
Dan Farrelly <daniel.j.farrelly@gmail.com>
2023-09-19 17:53:40 -0400

    Fix function export

--- inngest/index.ts
+++ inngest/index.ts
@@ -2,7 +2,7 @@ import * as payments from './payments';
 import * as emails from './emails';
 import * as test from './test';
 
-export const functions = [...Object.values(emails), ...Object.values(payments)];
+const functions = [...Object.values(emails), ...Object.values(payments)];
 
 // Only set the cron trigger to run in production, not branch envs
 if (
@@ -12,5 +12,7 @@ if (
   console.log('Not a pull request preview, adding test cron');
   functions.push(...Object.values(test));
 }
+
+export { functions };
 
 export { inngest } from './client';
