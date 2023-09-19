ea8cc155203ef1ebbb76e52109a58627ec94e24e
Dan Farrelly <daniel.j.farrelly@gmail.com>
2023-09-19 16:28:22 -0400

    Test self-register

--- index.ts
+++ index.ts
@@ -1,26 +1,35 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 
-import express from "express";
-import { serve } from "inngest/express";
+import express from 'express';
+import { serve } from 'inngest/express';
 
 dotenv.config();
 
-import { functions, inngest } from "./inngest";
+import { functions, inngest } from './inngest';
 
 const PORT = 3030;
 
 const app = express();
 
-console.log(functions[0]);
-
 app.use(express.json());
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.json({ success: true });
 });
 
-app.use("/api/inngest", serve(inngest, [...functions]));
+app.use('/api/inngest', serve(inngest, [...functions]));
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`✅ Server started on localhost:${PORT}
 ➡️ Inngest running at http://localhost:${PORT}/api/inngest`);
+
+  // Attempt to self-register the app after deploy
+  if (process.env.RENDER_EXTERNAL_URL) {
+    const inngestURL = new URL('/api/inngest', process.env.RENDER_EXTERNAL_URL);
+    const result = await fetch(inngestURL, {
+      method: 'PUT',
+    });
+    const json = await result.json();
+    console.log(`Register attempted:`);
+    console.log(result.status, json);
+  }
 });
