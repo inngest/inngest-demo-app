66a0bf804a9e6b0b403697248efda01b966fccd3
Dan Farrelly <daniel.j.farrelly@gmail.com>
2025-02-07 16:27:45 -0500

    Debug env

--- inngest/client.ts
+++ inngest/client.ts
@@ -2,11 +2,15 @@ import { Inngest, EventSchemas, InngestMiddleware } from 'inngest';
 
 import type { EventUnion } from './events';
 
+const env = ['main', 'staging'].includes(process.env.RENDER_GIT_BRANCH)
+  ? null
+  : process.env.RENDER_GIT_BRANCH;
+
+console.log('DEBUG env=', env);
+
 export const inngest = new Inngest({
   id: 'demo-app',
   schemas: new EventSchemas().fromUnion<EventUnion>(),
-  env: ['main', 'staging'].includes(process.env.RENDER_GIT_BRANCH)
-    ? null
-    : process.env.RENDER_GIT_BRANCH,
+  env,
   appVersion: process.env.RENDER_GIT_COMMIT || 'local',
 });
