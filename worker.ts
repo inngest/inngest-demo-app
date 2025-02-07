f1990af57cf945c2097c9807057a3cd1903fffc2
Dan Farrelly <daniel.j.farrelly@gmail.com>
2025-02-07 15:09:53 -0500

    Add health check server

--- worker.ts
+++ worker.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { connect } from 'inngest/connect';
+import express from 'express';
 import { getFunctions, inngest } from './inngest/index';
 
 const instanceId = process.env.RENDER_INSTANCE_ID || 'local';
@@ -11,71 +12,37 @@ console.log(
   `Worker: connecting (instanceId:${instanceId}, appVersion:${inngest.appVersion})`
 );
 
-connect(inngest, {
-  functions: getFunctions(),
-  instanceId: process.env.RENDER_INSTANCE_ID || 'local',
-}).then((connection) => {
-  console.log('Worker: connected');
-  connection.closed.then(() => {
-    console.log('Worker: disconnected');
+const PORT = 3030;
+
+function main() {
+  const app = express();
+  app.use(express.json());
+  app.get('/', (req, res) => {
+    res.json({ success: true, connectionStatus });
   });
-});
 
-// const PORT = 3030;
+  let connectionStatus = 'disconnected';
 
-// const app = express();
+  connect(inngest, {
+    functions: getFunctions(),
+    instanceId: process.env.RENDER_INSTANCE_ID || 'local',
+  })
+    .then((connection) => {
+      console.log('Worker: connected');
+      connectionStatus = 'connected';
+      connection.closed.then(() => {
+        console.log('Worker: disconnected');
+        connectionStatus = 'disconnected';
+      });
+    })
+    .catch((err) => {
+      console.log('Worker: failed to connect', err);
+    });
 
-// app.use(express.json());
-// app.get('/', (req, res) => {
-//   res.json({ success: true });
-// });
+  // Start the server for health checks
+  app.listen(PORT, async () => {
+    console.log(`✅ Server started on localhost:${PORT}`);
+  });
+}
 
-// const functions = getFunctions();
-
-// app.use(
-//   '/api/inngest',
-//   serve({
-//     client: inngest,
-//     functions,
-//   })
-// );
-
-// app.listen(PORT, async () => {
-//   console.log(`✅ Server started on localhost:${PORT}
-// ➡️ Inngest running at http://localhost:${PORT}/api/inngest`);
-
-//   // Attempt to self-register the app after deploy
-//   if (process.env.RENDER_EXTERNAL_URL) {
-//     console.log(
-//       `Attempting self-register. Functions: `,
-//       functions.map((f) => f.name).join(', ')
-//     );
-//     const inngestURL = new URL('/api/inngest', process.env.RENDER_EXTERNAL_URL);
-//     const result = await fetch(inngestURL, {
-//       method: 'PUT',
-//     });
-//     await sleep(2000);
-//     try {
-//       const json = await result.json();
-//       console.log(
-//         `Register attempted:`,
-//         inngestURL.toString(),
-//         result.status,
-//         json
-//       );
-//     } catch (err) {
-//       console.log(
-//         `Register failed:`,
-//         inngestURL.toString(),
-//         result.status,
-//         result.body
-//       );
-//     }
-//   }
-// });
-
-// function sleep(t: number): Promise<void> {
-//   return new Promise((res) => {
-//     return setTimeout(res, t);
-//   });
-// }
+main();
